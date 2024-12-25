b589a223ab134fc779f4a02036968568f70642e1
Mohnish <mohnish.a007@gmail.com>
2024-12-25 03:25:34 +0000

    bg video updated

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,82 @@
+'use client'
+import React from 'react'
+import { useState } from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import Logo from '../../../public/logo/logo-removebg-preview.png'
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
+// TODO: Navbar complted from functionality but make it crisp
+function Navbar() {
+
+    const [menuIcon, setmenuIcon] = useState(false)
+    const smallNav = () => { // we are toggeling the menuIcon state here
+        setmenuIcon(!menuIcon)
+    }
+
+  return (
+    <>
+    <header className=' text-blue-500 w-full ease-in duration-300 fixed top-0 left-0 z-10'>
+        <nav className='max-w-[1366px] mx-auto h-[100px] flex justify-between items-center p-4'>
+            <div>
+                <Link href='/' onClick={smallNav}>
+                <Image src={Logo}
+                alt='UNCMON Logo'
+                width={150}
+                height={40}
+                />
+                </Link>
+               
+            </div>
+             {/* large screen nav */}
+             {/* This nav links are hidden but after md size they will show up */}
+             <ul className='hidden md:flex lg:text-[20px] uppercase text-slate-900'>
+                    <li className='mr-4 lg:mr-8 text-[#ffdc73] hover:text-[#00df9a]'><Link href='/' onClick={smallNav}>Home</Link></li>
+                    <li className='mr-4 lg:mr-8 text-[#ffdc73] hover:text-[#00df9a]'><Link href='/About' onClick={smallNav}>About</Link></li>
+                    <li className='mr-4 lg:mr-8 text-[#ffdc73] hover:text-[#00df9a]'><Link href='/Behindthescene' onClick={smallNav}>Behind the scene</Link></li>
+                    {/* <li className='mr-4 lg:mr-8 text-[#ffdc73] hover:text-[#00df9a]'><Link href='/Services' onClick={smallNav}>Services</Link></li> */}
+                    <li className='mr-4 lg:mr-8 text-[#ffdc73] hover:text-[#00df9a]'><Link href='/Contact' onClick={smallNav}>Contact</Link></li>
+                </ul>
+
+
+            {/* small screen nav */}
+            {/* Onclick change the icons */}
+            <div className='flex md:hidden' onClick={smallNav}>
+                {menuIcon ? (<AiOutlineClose size={30} className='text-[#ffdc73] hover:text-[#00df9a]' onClick={smallNav} />):(<AiOutlineMenu size={30} className='text-[#ffdc73] hover:text-[#00df9a]' onClick={smallNav} />)}  
+            </div>
+            {/* Smaller screen navbar */}
+            <div className={
+                menuIcon ? 
+                'md:hidden  absolute top-[100px] right-0 bottom-0 left-0 flex justify-center items-center w-full h-screen ease-in duration-300 bg-slate-900 text-slate-300'
+                :
+                'md:hidden  absolute top-[100px] right-0 bottom-0 left-[-100%]  flex justify-center items-center w-full h-screen ease-in duration-300 bg-slate-900 text-slate-300 text-center'
+            }>
+
+                {/* small screen navlinks */}
+                <div className='w-full'>
+                    <ul className='uppercase text-2xl text-center font-bold'>
+                        <li onClick={smallNav} className='py-5 hover:text-[#00df9a]'>
+                            <Link href='/About'>About</Link>
+                        </li>
+                        <li onClick={smallNav} className='py-5 hover:text-[#00df9a]'>
+
+                            <Link href='/Behindthescene'>Behind the scene</Link>
+                        </li>
+                        {/* <li onClick={smallNav} className='py-5 hover:text-[#00df9a]'>
+
+                            <Link href='/Services'>Services</Link>
+                        </li> */}
+                        <li onClick={smallNav} className='py-5 hover:text-[#00df9a]'>
+                            <Link href='/Contact'>Contact</Link>
+                            
+                        </li>
+                    </ul>
+
+                </div>
+            </div>
+        </nav>
+    </header>
+    </>
+  )
+}
+
+export default Navbar
---
+++ src/app/components/bgvideo.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Link from 'next/link'
+function Bgvideo() {
+  return (
+    <>
+     <div className="container relative flex items-center justify-center flex-col">
+    {/* <img className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600"> */}
+    {/* <Image
+      src="/images/opscraftherosection.png"
+      width={720}
+      height={600}
+      alt="Picture of the author"
+    /> */}
+    <div className='w-screen '>
+
+    <video src="hduni.mp4" autoPlay loop muted playsInline  className='md:min-w-[110%] min-w-full  min-h-full '/>
+    </div>
+    <div className="sm:text-center sm:items-center sm:justify-center text-center items-center justify-center absolute sm:w-4/5 w-screen">
+    
+      <h1 className="title-font font-bold sm:text-2xl text-sm  text-slate-200"> CRAFTING EXTRAORDINARY BRAND STORIES</h1>
+      <p className="text-sm sm:text-xl leading-relaxed text-slate-100">A creative studio that create, produce and deliver beautifully <br /> crafted  animation content that transforms the world&apos;s greatest brands. </p>
+      <div className="flex justify-center sm:justify-center">
+        <Link href='/Behindthescene'>
+        <button className=" mt-4 text-white hover:bg-[#ffdc73] border-0 py-2 px-6 focus:outline-none bg-indigo-600 rounded text-lg">View More</button>
+        </Link>
+        {/* <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Button</button> */}
+      </div>
+    
+    </div>
+  </div>
+    </>
+  )
+}
+
+export default Bgvideo

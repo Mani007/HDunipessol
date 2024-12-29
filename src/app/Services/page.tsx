d5ea447144728041273ca9987d8a51e4ec66edb1
Mohnish <mohnish.a007@gmail.com>
2024-12-29 01:32:45 +0000

    card amd service templates are mobile ready

--- src/app/Services/page.tsx
+++ src/app/Services/page.tsx
@@ -5,16 +5,17 @@ function page() {
   return (
     <>
       <div className="">
-        <div className="pt-40  bg-[#10375C] flex flex-row gap-3 px-3">
-          <div className=" rounded-lg ">
+        <div className="pt-28  bg-[#10375C] sm:flex sm:flex-row flex-col  gap-3 px-3">
+          <div className="pt-3 rounded-lg ">
             <Card />
           </div>
-          <div className=" rounded-lg ">
+          <div className="pt-3 rounded-lg ">
             <Card />
           </div>
-          <div className=" rounded-lg ">
+          <div className="pt-3 rounded-lg ">
             <Card />
           </div>
+         
           {/* <div className=""> 
           <p>Lorem ipsum dolor sit amet.</p>
         </div>
@@ -28,6 +29,18 @@ function page() {
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, fugiat.</p>
         </div> */}
         </div>
+        <div className="bg-[#10375C]  pt-4 sm:flex sm:flex-row flex-col gap-3 px-3">
+          <div className=" pt-3 rounded-lg ">
+            <Card />
+          </div>
+          <div className="pt-3 rounded-lg ">
+            <Card />
+          </div>
+          <div className="pt-3 rounded-lg ">
+            <Card />
+          </div>
+
+          </div>
       </div>
     </>
   );
--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -5,7 +5,7 @@ import Service1 from "../../../public/Services/services1.jpg";
 function Card() {
   return (
     <>
-    <div className="relative  rounded-lg shadow transition hover:shadow-lg">
+    <div className="relative  rounded-lg  shadow transition hover:shadow-lg">
   <Image
     alt="Service"
     src={Service1}
@@ -15,20 +15,21 @@ function Card() {
     className="absolute inset-0 h-full w-full object-cover"
   />
 
-  <div className="relative flex flex-col items-center  pt-32 sm:pt-48 lg:pt-64">
-    <div className="p-4 sm:p-6">
+  <div className="relative flex flex-col items-center justify-center  pt-32 sm:pt-48 lg:pt-64">
+    <div className="p-4  items-center justify-center sm:p-6">
       
 
-     <h2 className='text-[#ffdc73]  '>Accounting</h2>
+     <h2 className='text-[#ffdc73] text-center '>Accounting</h2>
 
-      <p className="mt-2 line-clamp-3 text-sm/relaxed text-[#ffdc73] ">
+      <p className="mt-2 line-clamp-3 text-center text-sm/relaxed text-[#ffdc73] ">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae dolores, possimus
         pariatur animi temporibus nesciunt praesentium dolore sed nulla ipsum eveniet corporis
         quidem, mollitia itaque minus soluta, voluptates neque explicabo tempora nisi culpa eius
         atque dignissimos. Molestias explicabo corporis voluptatem?
       </p>
-      <Link href="/Contact">
-        <button className="mt-4 text-white hover:bg-[#ffdc73] border-0 py-2 px-6 focus:outline-none bg-[#00df9a] rounded text-lg">Contact</button>
+
+      <Link href="/Contact" className='flex items-center justify-center'>
+        <button className="mt-4 text-white  hover:bg-[#ffdc73] border-0 py-2 px-6 focus:outline-none bg-[#00df9a] rounded text-lg">Contact</button>
       </Link>
     </div>
   </div>

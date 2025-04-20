0eac99d8d0bfa8d4bea126ea401fa7818435a194
Mohnish <mohnish.a007@gmail.com>
2025-04-20 14:34:38 +0100

    Contact page: updates

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -4,11 +4,12 @@ import Map from '../components/Map'
 function page() {
   return (
    <>
-   <div className="w-full relative text-gray-700 bg-[#10375C] body-font">
-    <div className="container flex flex-col flex-wrap px-5 py-24 mx-auto md:items-center lg:items-start md:flex-row md:flex-no-wrap">
-      <div className="flex-shrink-0 w-64 mx-auto  text-center md:mx-0 md:text-left">
+   <div className="w-full relative text-gray-700 bg-[#10375C]  body-font">
+    <div className="container flex flex-col bg-[#10375C] flex-wrap px-5 py-24 mx-auto md:items-center lg:items-start md:flex-row md:flex-no-wrap">
+      <div className="flex-shrink-0 w-0 mx-auto mt-8 text-center md:mx-0 md:text-left">
      <Map/>
       </div>
+      
     </div>
    </div>
    </>

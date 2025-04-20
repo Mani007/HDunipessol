02fad07877af233654e6f708de67fde676ebc6cd
Mohnish <mohnish.a007@gmail.com>
2025-04-20 18:02:32 +0100

    Contact page: flex readjusted

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -5,11 +5,14 @@ function page() {
   return (
    <>
    <div className="w-full relative text-gray-700 bg-[#10375C]  body-font">
-    <div className="container flex flex-col bg-[#10375C] flex-wrap px-5 py-24 mx-auto md:items-center lg:items-start md:flex-row md:flex-no-wrap">
-      <div className="flex-shrink-0 w-0 mx-auto mt-8 text-center md:mx-0 md:text-left">
-     <Map/>
-      </div>
+    <div className="container  flex flex-col bg-[#10375C] flex-wrap px-5 py-24 mx-auto md:items-center lg:items-start md:flex-row md:flex-no-wrap">
       
+      <div className="flex lg:flex-row ">
+        <p className='text-6xl bg-red-500'>Some text</p>
+      </div>
+      <div className="flex-shrink-0 w-0 mx-auto ">
+      <Map/>
+      </div>
     </div>
    </div>
    </>

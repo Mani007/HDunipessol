00eb1e45bc81ec4ecef5516f37a798670336449c
Mohnish <mohnish.a007@gmail.com>
2025-04-21 02:14:43 +0100

    Contact page: All basic linking ready

--- src/app/components/bgvideo.tsx
+++ src/app/components/bgvideo.tsx
@@ -20,8 +20,8 @@ function Bgvideo() {
       <h1 className="mt-20 title-font font-bold sm:text-2xl text-sm  text-[#ffdc73]"> CRAFTING EXTRAORDINARY BRAND STORIES</h1>
       <p className="text-sm sm:text-xl leading-relaxed text-[#ffdc73]">A creative studio that create, produce and deliver beautifully <br /> crafted  animation content that transforms the world&apos;s greatest brands. </p>
       <div className="flex justify-center sm:justify-center">
-        <Link href='/Behindthescene'>
-        <button className=" mt-4 text-white hover:bg-[#ffdc73] border-0 py-2 px-6 focus:outline-none bg-[#00df9a] rounded text-lg">View More</button>
+        <Link href='/Expertise'>
+        <button className=" mt-4 text-white hover:bg-[#ffdc73] border-0 py-2 px-6 focus:outline-none bg-[#00df9a] rounded text-lg">Know More</button>
         </Link>
         {/* <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Button</button> */}
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 
 import Bgvideo from "./components/bgvideo";
+// import About from "./About/page"
+// import Services from "./Services/page"
 
 export default function Home() {
   return (
     <>
     <Bgvideo/>
+    
+    {/* <About/> */}
     </>
   );
 }
